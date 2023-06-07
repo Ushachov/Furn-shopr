e787c5b80b3aacaf3ea973494778588927a6dccf
Ivan Ushachov <usacovg@gmail.com>
2023-06-07 14:32:49 +0300

    test

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,7 +1,6 @@
 import fileInclude from "gulp-file-include";
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
 import versionNumber from "gulp-version-number";
-//import pug from "gulp-pug";
 
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
